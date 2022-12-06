088e434a4018ecc415f88fc292d959a9f06cae41
Bruna Domingues Leite <brunadomingues.dev@gmail.com>
2022-12-06 20:16:07 -0300

    Iniciando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components';
+import { Button } from "./components/Button";
+import { defaultTheme } from './styles/themes/default';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
+    </ThemeProvider>
   )
 }
-
-export default App
